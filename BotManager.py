d53687f54ce30d507e40278b8e43b7ef22ebd70b
matcompeng <matcompeng@gmail.com>
2024-09-12 16:44:03 +0300

    set Trailing Percentage param

--- BotManager.py
+++ BotManager.py
@@ -36,7 +36,7 @@ AMOUNT_RSI_INTERVAL = '5m'      # Interval To get its RSI for Buying Amount Calc
 AMOUNT_ATR_INTERVAL = '15m'     # Interval To get its ATR for Buying Amount Calculations Function
 USDT_DIP_AMOUNT = 5             # Amount of Currency For Buying a Dip
 MIN_STABLE_INTERVALS = 4        # Set The Minimum Stable Intervals For Market Stable Condition
-TRAILING_POSITIONS_LEN = 5      # Define The Minimum Length For Stable Positions To start Gain Reversal Process
+TRAILING_POSITIONS_COUNT = 5    # Define The Minimum Count For Stable Positions To start Trailing Reversal Process
 TRAILING_PERCENT = 1.5          # Set The Minimum % To Activate Trailing Stop Process
 TRAILING_GAIN_REVERSE = 0.25    # Set the Sell Threshold % for Stable Portfolio Gain Reversal (Trailing Stop)
 CHECK_POSITIONS_ON_BUY = True   # Set True If You Need Bot Manager Check The Positions During Buy Cycle
@@ -279,7 +279,7 @@ class BotManager:
             stable_positions_len = len([position for position_id, position in self.position_manager.get_positions().items() if position['dip'] == 0])
             print(f"Stable Positions Count: {stable_positions_len}")
 
-            if stable_positions_len >= TRAILING_POSITIONS_LEN and self.decision_maker.calculate_stable_portfolio_gain(bot_manager, current_price) >= TRAILING_PERCENT:
+            if stable_positions_len >= TRAILING_POSITIONS_COUNT and self.decision_maker.calculate_stable_portfolio_gain(bot_manager, current_price) >= TRAILING_PERCENT:
                 reversed_decision = self.decision_maker.check_for_sell_due_to_reversal(bot_manager, current_price)
 
                 if reversed_decision == "Sell":
